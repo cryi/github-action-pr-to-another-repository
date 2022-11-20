e22c6071d22803cf78a6d5d03f0d960c10fb9ec3
V <36897290+cryi@users.noreply.github.com>
2022-11-20 22:25:22 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -145,6 +145,10 @@ echo "[+] git diff-index:"
 # git diff-index : to avoid doing the git commit failing if there are no changes to be commit
 git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
 
+echo "[+] Creating branch"
+git checkout -b "$TARGET_BRANCH"
+
 echo "[+] Pushing git commit"
+
 # --set-upstream: sets de branch when pushing to a branch that does not exist
 git push "$GIT_CMD_REPOSITORY" --set-upstream "$TARGET_BRANCH"
